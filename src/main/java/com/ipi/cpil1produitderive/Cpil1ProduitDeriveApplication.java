0a4c7aa7e919f4887646eeda0f365182a9f1d71a
Julien <kalaycijulien@gmail.com>
2023-04-06 08:58:46 +0200

    Création du projet

---
+++ src/main/java/com/ipi/cpil1produitderive/Cpil1ProduitDeriveApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.cpil1produitderive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cpil1ProduitDeriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cpil1ProduitDeriveApplication.class, args);
+    }
+
+}
