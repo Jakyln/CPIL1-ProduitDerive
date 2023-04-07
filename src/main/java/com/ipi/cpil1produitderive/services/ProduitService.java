11d3a3023aea825af484a98a33a2150ac623c05f
Jakyln <kalaycijulien@gmail.com>
2023-04-07 14:33:16 +0200

    début des tests

--- src/main/java/com/ipi/cpil1produitderive/services/ProduitService.java
+++ src/main/java/com/ipi/cpil1produitderive/services/ProduitService.java
@@ -2,12 +2,24 @@ package com.ipi.cpil1produitderive.services;
 
 import com.ipi.cpil1produitderive.dao.AdresseDAO;
 import com.ipi.cpil1produitderive.dao.ProduitDAO;
+import com.ipi.cpil1produitderive.models.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 
 public class ProduitService {
     @Autowired
     private ProduitDAO produitDAO;
+
+    public List<Map<String,Object>> getAllProduitStats(Produit produit) {
+        List<Map<String,Object>> allProduitStats = new ArrayList<>();
+        return allProduitStats;
+    }
+
 }
---
+++ src/test/java/com/ipi/cpil1produitderive/services/ProduitServiceTest.java
@@ -0,0 +1,26 @@
+package com.ipi.cpil1produitderive.services;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProduitServiceTest {
+
+    public void testGetAllProduitStats(){
+        //Given
+        List<Map<String,Object>> allProduitStats = new ArrayList<>();
+
+        //When
+        //Then
+    }
+
+    public void testGetOneProduitStat(){
+        //Given
+        Map<String,Object> produitStat = new HashMap<>();
+
+        //When
+        //Then
+    }
+}
