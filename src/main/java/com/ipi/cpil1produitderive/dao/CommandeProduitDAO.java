cae9a2e3ea1427893742811efce7eef8a7f1cc2c
Jakyln <kalaycijulien@gmail.com>
2023-06-14 16:21:46 +0200

    format des fonctions dao

--- src/main/java/com/ipi/cpil1produitderive/dao/CommandeProduitDAO.java
+++ src/main/java/com/ipi/cpil1produitderive/dao/CommandeProduitDAO.java
@@ -10,30 +10,88 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommandeProduitDAO extends JpaRepository<CommandeProduit, Long> {
     //Chiffre d'affaires
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixAchat) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 0", nativeQuery = true)
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixAchat) " +
+            "FROM commandeproduit " +
+            "INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 0", nativeQuery = true)
     Double getSumPrixAchatOfCommandeOffline();
 
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixAchat) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 1", nativeQuery = true)
+
+
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixAchat) " +
+            "FROM commandeproduit " +
+            "INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 1", nativeQuery = true)
     Double getSumPrixAchatOfCommandeOnline();
 
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixAchat) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 1 AND commandeproduit.idProduit = ?", nativeQuery = true)
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixAchat) " +
+            "FROM commandeproduit " +
+            "INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 1 " +
+            "AND commandeproduit.idProduit = ?", nativeQuery = true)
     Double getSumPrixAchatOfCommandeOnlineOfProduit(long idProduit);
 
 
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixAchat) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 0 AND commandeproduit.idProduit = ?", nativeQuery = true)
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixAchat) " +
+            "FROM commandeproduit INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 0 " +
+            "AND commandeproduit.idProduit = ?", nativeQuery = true)
     Double getSumPrixAchatOfCommandeOfflineOfProduit(long idProduit);
 
     //Marge Brut dégagé
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixVente) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 0", nativeQuery = true)
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixVente) " +
+            "FROM commandeproduit " +
+            "INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 0", nativeQuery = true)
     Double getSumPrixVenteOfCommandeOffline();
 
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixVente) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 1", nativeQuery = true)
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixVente) " +
+            "FROM commandeproduit INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 1", nativeQuery = true)
     Double getSumPrixVenteOfCommandeOnline();
 
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixVente) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 1 AND commandeproduit.idProduit = ?", nativeQuery = true)
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixVente) " +
+            "FROM commandeproduit INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 1 " +
+            "AND commandeproduit.idProduit = ?", nativeQuery = true)
     Double getSumPrixVenteOfCommandeOnlineOfProduit(long idProduit);
 
 
-    @Query(value = "SELECT SUM(commandeproduit.quantite * produit.prixVente) FROM commandeproduit INNER JOIN produit ON commandeproduit.idProduit = produit.id INNER JOIN commande ON commandeproduit.idCommande = commande.id WHERE commande.isPurchasedOnline = 0 AND commandeproduit.idProduit = ?", nativeQuery = true)
+    @Query(value = "SELECT " +
+            "SUM(commandeproduit.quantite * produit.prixVente) " +
+            "FROM commandeproduit INNER JOIN produit " +
+            "ON commandeproduit.idProduit = produit.id " +
+            "INNER JOIN commande " +
+            "ON commandeproduit.idCommande = commande.id " +
+            "WHERE commande.isPurchasedOnline = 0 " +
+            "AND commandeproduit.idProduit = ?", nativeQuery = true)
     Double getSumPrixVenteOfCommandeOfflineOfProduit(long idProduit);
 }
